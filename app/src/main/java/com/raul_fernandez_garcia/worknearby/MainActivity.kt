b02ed761b61166d30cea586e09e31c9b310fff47
jraul <jraulfernandezg@gmail.com>
2025-10-01 13:18:12 +0200

    Lista de ofertas

--- app/src/main/java/com/raul_fernandez_garcia/worknearby/MainActivity.kt
+++ app/src/main/java/com/raul_fernandez_garcia/worknearby/MainActivity.kt
@@ -9,15 +9,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
@@ -74,8 +79,6 @@ private fun Content() {
         }
     ) {
         Scaffold(
-            modifier = Modifier.padding(),
-
             topBar = {
                 CenterAlignedTopAppBar(
                     title = {
@@ -102,8 +105,8 @@ private fun Content() {
                     }
                 )
             }
-        ) {
-            ListaOfertas(modifier = Modifier.padding())
+        ) { paddingValues ->
+            ListaOfertas(modifier = Modifier.padding(paddingValues))
 
         }
     }
@@ -113,18 +116,31 @@ private fun Content() {
 @Composable
 private fun ListaOfertas(modifier: Modifier = Modifier) {
     LazyColumn(
-        modifier = Modifier
+        modifier
             .background(Color.Gray)
             .fillMaxSize()
             .padding(PaddingValues())
     ) {
         items(5) { index ->
-            Text(
-                text = "Item: $index",
-                fontSize = 28.sp,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center
-            )
+
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                ),
+                modifier = Modifier
+                    .height(150.dp)
+                    .padding(15.dp)
+                    .fillMaxWidth()
+
+            ) {
+                Text(
+                    text = "Item: $index",
+                    fontSize = 28.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+            }
         }
+
     }
 }
