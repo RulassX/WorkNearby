912395829725f584b6291d5fa9a79398998b5eb7
jraul <jraulfernandezg@gmail.com>
2025-10-03 13:37:33 +0200

    Lista de ofertas

--- app/src/main/java/com/raul_fernandez_garcia/worknearby/MainActivity.kt
+++ app/src/main/java/com/raul_fernandez_garcia/worknearby/MainActivity.kt
@@ -14,10 +14,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -38,7 +36,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -63,17 +60,17 @@ class MainActivity : ComponentActivity() {
 private fun app() {
     WorkNearbyTheme {
         Surface() {
-            Content()
-
+            BuscarOfertas()
+            BuscarContratos()
         }
     }
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
+@Preview
 @Composable
-private fun Content() {
+private fun BuscarOfertas() {
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
@@ -116,7 +113,6 @@ private fun Content() {
             }
         ) { paddingValues ->
             ListaOfertas(modifier = Modifier.padding(paddingValues))
-
         }
     }
 }
@@ -174,6 +170,126 @@ private fun ListaOfertas(modifier: Modifier = Modifier) {
 
                         Text(
                             text = "Valoracion: " + "5/5",
+                            fontSize = 17.sp,
+                            modifier = Modifier
+                                .padding(start = 15.dp, bottom = 15.dp),
+                            textAlign = TextAlign.End,
+                        )
+                    }
+                    Image(
+                        modifier = Modifier
+                            .padding(15.dp),
+                        painter = painterResource(id = R.drawable.ic_launcher_background),
+                        contentDescription = "imagen",
+                    )
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+private fun BuscarContratos() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text(text = "OP1", modifier = Modifier.padding(16.dp))
+                Text(text = "OP2", modifier = Modifier.padding(16.dp))
+                Text(text = "OP3", modifier = Modifier.padding(16.dp))
+            }
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(
+                            text = "Contratos"
+                        )
+
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                if (drawerState.isClosed) {
+                                    drawerState.open()
+                                } else {
+                                    drawerState.close()
+                                }
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Abrir menu"
+                            )
+                        }
+                    }
+                )
+            }
+        ) { paddingValues ->
+            ListaContratos(modifier = Modifier.padding(paddingValues))
+        }
+    }
+}
+
+@Composable
+private fun ListaContratos(modifier: Modifier = Modifier) {
+    val nombres = listOf("Raúl", "Brais", "Laura", "Carlos", "Lucia", "Pedro", "Maria")
+    val apellidos =
+        listOf("Fernández García", "Fernández", "Gomez", "Varela", "Rodriguez", "Lopez", "García")
+
+    LazyColumn(
+        modifier
+            .background(Color.Gray)
+            .fillMaxSize()
+            .padding(PaddingValues())
+            .padding(top = 10.dp)
+    ) {
+        items(nombres.zip(apellidos)) { (nombre, apellido) ->
+
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                ),
+                modifier = Modifier
+                    .height(150.dp)
+                    .padding(vertical = 10.dp, horizontal = 15.dp)
+                    .fillMaxSize()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(4.dp),
+                        modifier = Modifier.fillMaxHeight(),
+                    ) {
+                        Text(
+                            text = "Nombre: " + nombre,
+                            fontSize = 17.sp,
+                            modifier = Modifier
+                                .padding(start = 15.dp, top = 15.dp),
+                            //textAlign = TextAlign.Center,
+                        )
+                        Text(
+                            text = "Apellidos: " + apellido,
+                            fontSize = 17.sp,
+                            modifier = Modifier
+                                .padding(start = 15.dp, top = 5.dp),
+                            //textAlign = TextAlign.Center,
+                        )
+
+                        Spacer(modifier = Modifier.weight(1f))
+
+                        Text(
+                            text = "Trabajo: " + "Pintor",
                             fontSize = 17.sp,
                             modifier = Modifier
                                 .padding(start = 15.dp, bottom = 15.dp),
