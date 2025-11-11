402c3eb515deaa14203df62f0657ea79e4dbad01
jraul <jraulfernandezg@gmail.com>
2025-11-11 17:41:19 +0100

    api service

---
+++ app/src/main/java/com/raul_fernandez_garcia/worknearby/ApiService.kt
@@ -0,0 +1,16 @@
+package com.raul_fernandez_garcia.worknearby
+
+import com.raul_fernandez_garcia.worknearby.modelo.Usuario
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @POST("usuarios/crear")
+    fun crearUsuario(@Body usuario: Usuario): Call<Usuario>
+
+    @GET("usuarios")
+    fun obtenerUsuarios(): Call<List<Usuario>>
+}
