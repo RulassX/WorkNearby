536192412598ce2fe77d58032f35501a674f8aaa
jraul <jraulfernandezg@gmail.com>
2025-11-13 12:44:18 +0100

    retrofit created

--- app/src/main/java/com/raul_fernandez_garcia/worknearby/RetrofitClient.kt
+++ app/src/main/java/com/raul_fernandez_garcia/worknearby/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/" // o tu IP local si es dispositivo real
+    private const val BASE_URL = "http://192.168.1.138:8080/"
 
     val instance: ApiService by lazy {
         Retrofit.Builder()
